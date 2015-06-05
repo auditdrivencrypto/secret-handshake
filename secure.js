0d35105b06d249f246f48a090ca3cb6d9c2950e3
Dominic Tarr <dominic.tarr@gmail.com>
2015-06-05 22:20:00 +0200

    make securing a stream a separate api

---
+++ secure.js
@@ -0,0 +1,28 @@
+var sodium = require('sodium').api
+var hash = sodium.crypto_hash_sha256
+var pull = require('pull-stream')
+var boxes = require('pull-box-stream')
+
+var concat = Buffer.concat
+
+module.exports = function (pub, cb) {
+
+  return function (err, stream, secret, remote_pub) {
+    if(err) return cb(err)
+
+    var encrypt = hash(concat([secret, remote_pub]))
+    var decrypt = hash(concat([secret, pub]))
+
+    console.log('EN', encrypt)
+    console.log('DE', decrypt)
+
+
+    cb(null, {
+      source: pull(stream.source, boxes.createUnboxStream(decrypt)),
+      sink: pull(boxes.createBoxStream(encrypt), stream.sink)
+    })
+  }
+
+}
+
+
