01130471fd831707644661e9a9f886c5f54bceba
Dominic Tarr <dominic.tarr@gmail.com>
2015-06-05 22:20:45 +0200

    test with secret-handshake simply returning the secret, and the stream

--- test/secret-handshake.js
+++ test/secret-handshake.js
@@ -1,8 +1,7 @@
 
-var handshake = require('../')
+var shs = require('../')
 
 var tape = require('tape')
-var boxes = require('pull-box-stream')
 var pull = require('pull-stream')
 
 var sodium = require('sodium').api
@@ -11,48 +10,39 @@ var deepEqual = require('deep-equal')
 var alice = sodium.crypto_sign_keypair()
 var bob   = sodium.crypto_sign_keypair()
 
-function secureStream (stream) {
-  return function (encrypt, decrypt) {
-    return {
-      source: pull(stream.source, boxes.createBoxStream(encrypt)),
-      sink: pull(boxes.createUnboxStream(decrypt), stream.sink)
-    }
-  }
-}
-
-function secureClient(clientKey, serverPublicKey, stream) {
-  return handshake.client(
-    clientKey,
-    serverPublicKey,
-    secureStream(stream)
-  )
-}
-
-function secureServer(serverKey, authenticate, stream) {
-  return handshake.server(
-    serverKey,
-    authenticate,
-    secureStream(stream)
-  )
-}
-
-
-var pair = require('pull-pair')
+var secure = require('../secure')
 
 tape('test handshake', function (t) {
 
-  var aliceHS = secureClient(alice, bob.publicKey, {
-    source: pull.values([new Buffer('hello there')]),
-    sink: pull.collect(function (err, hello_there) {
-      t.equal(hello_there.toString(), 'hello there')
-      console.log('output:', hello_there.join(''))
-      t.end()
-    })
-  })
+  var aliceHS = shs.client(alice, bob.publicKey,
+    secure(alice.publicKey, function (err, stream) {
 
-  var bobHS = secureServer(bob, function (pub, cb) {
-    cb(deepEqual(pub, alice.pub) && new Error('unauthorized'))
-  }, pair())
+      if(err) throw err
+
+      pull(
+        pull.values([new Buffer('hello there')]),
+        stream,
+        pull.collect(function (err, hello_there) {
+          t.equal(hello_there.toString(), 'hello there')
+          console.log('output:', hello_there.join(''))
+          t.end()
+        })
+      )
+
+    }))
+
+  var bobHS = shs.server(bob, function (pub, cb) {
+    console.log('AUTHORIZED?', pub, alice.publicKey)
+    t.deepEqual(pub, alice.publicKey)
+    cb(deepEqual(pub, alice.publicKey) ? null : new Error('unauthorized') )
+  }, secure(bob.publicKey, function (err, stream) {
+
+    if(err) throw err
+
+    pull(stream, pull.through(function (data) {
+      console.log(data.toString())
+    }), stream) //ECHO
+  }))
 
   pull(
     aliceHS,
