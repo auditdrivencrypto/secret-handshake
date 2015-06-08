54ca57504995b514eaa44315c9275c1fa90a6a1d
Dominic Tarr <dominic.tarr@gmail.com>
2015-06-08 13:24:31 +0200

    test callback error when a bit is flipped in any packet

--- test/secret-handshake.js
+++ test/secret-handshake.js
@@ -6,6 +6,7 @@ var pull = require('pull-stream')
 
 var sodium = require('sodium').api
 var deepEqual = require('deep-equal')
+var bitflipper = require('pull-bitflipper')
 
 var alice = sodium.crypto_sign_keypair()
 var bob   = sodium.crypto_sign_keypair()
@@ -14,10 +15,12 @@ var secure = require('../secure')
 
 var app_key = require('crypto').randomBytes(32)
 
+var createAlice = 
+
+
 tape('test handshake', function (t) {
 
-  var aliceHS =
-    shs.client(alice, app_key)
+  var aliceHS = shs.client(alice, app_key)
     (bob.publicKey, secure(function (err, stream) {
 
       if(err) throw err
@@ -60,3 +63,73 @@ tape('test handshake', function (t) {
   )
 
 })
+
+function bitflipTest(t, test) {
+  var errs = 0
+  var aliceHS = shs.client(alice, app_key)
+    (bob.publicKey, function (err) {
+      t.ok(err, 'Alice errored')
+      if(++errs === 2) t.end()
+    })
+
+  var bobHS = shs.server(bob, function (public, cb) {
+      t.deepEqual(public, alice.publicKey)
+
+      if(deepEqual(public, alice.publicKey)) cb(null)
+      else
+        cb(new Error('unauthorized'))
+
+    }, app_key) (function (err) {
+      t.ok(err, 'Bob errored')
+      if(++errs === 2) t.end()
+    })
+
+  test(aliceHS, bobHS)
+
+}
+
+tape('test auth fails when first packet is flipped', function (t) {
+  bitflipTest(t, function (aliceHS, bobHS) {
+    pull(
+      aliceHS,
+      bitflipper(1),
+      bobHS,
+      aliceHS
+    )
+  })
+})
+
+tape('test auth fails when 2nd packet is flipped', function (t) {
+  bitflipTest(t, function (aliceHS, bobHS) {
+    pull(
+      aliceHS,
+      bobHS,
+      bitflipper(1),
+      aliceHS
+    )
+  })
+})
+
+tape('test auth fails when 3rd packet is flipped', function (t) {
+  bitflipTest(t, function (aliceHS, bobHS) {
+    pull(
+      aliceHS,
+      bitflipper(2),
+      bobHS,
+      aliceHS
+    )
+  })
+})
+
+tape('test auth fails when 4th packet is flipped', function (t) {
+  bitflipTest(t, function (aliceHS, bobHS) {
+    pull(
+      aliceHS,
+      bobHS,
+      bitflipper(2),
+      aliceHS
+    )
+  })
+})
+
+
