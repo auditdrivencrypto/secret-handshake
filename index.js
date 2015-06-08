ed0497397260449bb92be7a8d32076afa99c6a33
Dominic Tarr <dominic.tarr@gmail.com>
2015-06-08 13:39:22 +0200

    handle errors

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ exports.createClientStream = function (alice, app_key) {
   return function (bob_pub, cb) {
     var state = new State(app_key, alice, bob_pub)
 
-    var stream = Handshake()
+    var stream = Handshake(cb)
     var shake = stream.handshake
     delete stream.handshake
 
@@ -26,13 +26,13 @@ exports.createClientStream = function (alice, app_key) {
     shake.read(challenge_length, function (err, msg) {
       //create the challenge first, because we need to generate a local key
       if(!state.verifyChallenge(msg))
-        return cb(new Error('wrong protocol (version?)'))
+        return shake.abort(new Error('wrong protocol (version?)'), cb)
 
       shake.write(state.createClientAuth())
 
       shake.read(server_auth_length, function (err, boxed_sig) {
         if(!state.verifyServerAccept(boxed_sig))
-          return cb(new Error('server not authenticated'))
+          return shake.abort(new Error('server not authenticated'), cb)
 
         cb(null, shake.rest(), state.cleanSecrets())
       })
@@ -48,23 +48,23 @@ exports.createServerStream = function (bob, authorize, app_key) {
 
   return function (cb) {
     var state = new State(app_key, bob)
-    var stream = Handshake()
+    var stream = Handshake(cb)
 
     var shake = stream.handshake
     delete stream.handshake
 
     shake.read(challenge_length, function (err, challenge) {
       if(!state.verifyChallenge(challenge))
-        return cb(new Error('wrong protocol/version'))
+        return shake.abort(new Error('wrong protocol/version'), cb)
 
       shake.write(state.createChallenge())
       shake.read(client_auth_length, function (err, hello) {
         if(!state.verifyClientAuth(hello))
-          return cb(new Error('unauthenticated client'))
+          return shake.abort(new Error('unauthenticated client'), cb)
 
         //check if the user wants to speak to alice.
         authorize(state.remote.public, function (err) {
-          if(err) throw err
+          if(err) return shake.abort(err, cb)
           shake.write(state.createServerAccept())
           cb(null, shake.rest(), state.cleanSecrets())
         })
