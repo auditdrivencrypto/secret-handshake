ffc92a87eb70974537bf25da0f8a373952a0f280
Dominic Tarr <dominic.tarr@gmail.com>
2015-06-17 15:33:43 +0200

    handle when the connection drops

--- handshake.js
+++ handshake.js
@@ -1,40 +1,97 @@
-
-var Reader = require('pull-reader')
+var sodium = require('sodium').api
 var pull = require('pull-stream')
-var deferred = require('pull-defer')
-var Writer = require('pull-pushable')
-var cat = require('pull-cat')
-var pair = require('pull-pair')
 
-module.exports = function (_cb) {
+var Handshake = require('pull-handshake')
+var State = require('./state')
 
-  var reader = Reader()
-  var writer = Writer(function (err) {
-    if(err) _cb(err)
-  })
+var challenge_length = 64
+var client_auth_length = 16+32+64
+var server_auth_length = 16+64
+var mac_length = 16
 
-  var source = deferred.source()
+//client is Alice
+//create the client stream with the public key you expect to connect to.
+exports.client =
+exports.createClientStream = function (alice, app_key) {
 
-  var p = pair()
+  return function (bob_pub, cb) {
+    var state = new State(app_key, alice, bob_pub)
 
-  return {
-    handshake: {
-      read: reader.read,
-      abort: function (err) {
-        reader.abort(err, function (err) {
-        })
-        _cb(err)
-      },
-      write: writer.push,
-      rest: function () {
-        writer.end()
-        return {
-          source: reader.read(),
-          sink: p.sink
-        }
-      }
-    },
-    sink: reader,
-    source: cat([writer, p.source])
+    var stream = Handshake(cb)
+    var shake = stream.handshake
+    delete stream.handshake
+
+    function abort(err, reason, cb) {
+      if(err && err !== true) shake.abort(err, cb)
+      else                    shake.abort(new Error(reason), cb)
+    }
+
+    shake.write(state.createChallenge())
+
+    shake.read(challenge_length, function (err, msg) {
+      if(err) return abort(err, 'challenge not accepted', cb)
+      //create the challenge first, because we need to generate a local key
+      console.log(msg)
+      if(!state.verifyChallenge(msg))
+        return abort(null, 'wrong protocol (version?)', cb)
+
+      shake.write(state.createClientAuth())
+
+      shake.read(server_auth_length, function (err, boxed_sig) {
+        if(err) return abort(err, 'hello not accepted', cb)
+
+        if(!state.verifyServerAccept(boxed_sig))
+          return abort(null, 'server not authenticated', cb)
+
+        cb(null, shake.rest(), state.cleanSecrets())
+      })
+    })
+
+    return stream
   }
 }
+
+//server is Bob.
+exports.server =
+exports.createServerStream = function (bob, authorize, app_key) {
+
+  return function (cb) {
+    var state = new State(app_key, bob)
+    var stream = Handshake(cb)
+
+    var shake = stream.handshake
+    delete stream.handshake
+
+    function abort (err, reason, cb) {
+      if(err && err !== true) shake.abort(err, cb)
+      else                    shake.abort(new Error(reason), cb)
+    }
+
+    shake.read(challenge_length, function (err, challenge) {
+      if(err) return abort(err, 'expected challenge', cb)
+      if(!state.verifyChallenge(challenge))
+        return shake.abort(new Error('wrong protocol/version'), cb)
+
+      shake.write(state.createChallenge())
+      shake.read(client_auth_length, function (err, hello) {
+        if(err) return abort(err, 'expected hello', cb)
+        if(!state.verifyClientAuth(hello)) {
+          //we know who the client was, but chose not to answer:
+          if(state.remote.public)
+            return abort(null, 'unauthenticated client:' + state.remote.public.toString('hex'), cb)
+          //client dialed wrong number... (we don't know who they where)
+          else
+            return abort(null, 'wrong number', cb)
+        }
+        //check if the user wants to speak to alice.
+        authorize(state.remote.public, function (err) {
+          if(err) return abort(err, 'client authentication rejected', cb)
+          shake.write(state.createServerAccept())
+          cb(null, shake.rest(), state.cleanSecrets())
+        })
+      })
+    })
+    return stream
+  }
+}
+
--- index.js
+++ index.js
@@ -1,76 +1,23 @@
-var sodium = require('sodium').api
-var pull = require('pull-stream')
+var handshake = require('./handshake')
+var secure = require('./secure')
 
-var Handshake = require('./handshake')
-var State = require('./state')
-
-var challenge_length = 64
-var client_auth_length = 16+32+64
-var server_auth_length = 16+64
-var mac_length = 16
-
-//client is Alice
-//create the client stream with the public key you expect to connect to.
 exports.client =
-exports.createClientStream = function (alice, app_key) {
+exports.createClient = function (alice, app_key) {
+  var create = handshake.client(alice, app_key)
 
-  return function (bob_pub, cb) {
-    var state = new State(app_key, alice, bob_pub)
-
-    var stream = Handshake(cb)
-    var shake = stream.handshake
-    delete stream.handshake
-
-    shake.write(state.createChallenge())
-
-    shake.read(challenge_length, function (err, msg) {
-      //create the challenge first, because we need to generate a local key
-      if(!state.verifyChallenge(msg))
-        return shake.abort(new Error('wrong protocol (version?)'), cb)
-
-      shake.write(state.createClientAuth())
-
-      shake.read(server_auth_length, function (err, boxed_sig) {
-        if(!state.verifyServerAccept(boxed_sig))
-          return shake.abort(new Error('server not authenticated'), cb)
-
-        cb(null, shake.rest(), state.cleanSecrets())
-      })
-    })
-
-    return stream
+  return function (bob, cb) {
+    return create(bob, secure(cb))
   }
-}
 
-//server is Bob.
+}
 exports.server =
-exports.createServerStream = function (bob, authorize, app_key) {
+exports.createServer = function (bob, authorize, app_key) {
+  var create = handshake.server(bob, authorize, app_key)
 
   return function (cb) {
-    var state = new State(app_key, bob)
-    var stream = Handshake(cb)
-
-    var shake = stream.handshake
-    delete stream.handshake
-
-    shake.read(challenge_length, function (err, challenge) {
-      if(!state.verifyChallenge(challenge))
-        return shake.abort(new Error('wrong protocol/version'), cb)
-
-      shake.write(state.createChallenge())
-      shake.read(client_auth_length, function (err, hello) {
-        if(!state.verifyClientAuth(hello))
-          return shake.abort(new Error('unauthenticated client'), cb)
-
-        //check if the user wants to speak to alice.
-        authorize(state.remote.public, function (err) {
-          if(err) return shake.abort(err, cb)
-          shake.write(state.createServerAccept())
-          cb(null, shake.rest(), state.cleanSecrets())
-        })
-      })
-    })
-    return stream
+    return create(secure(cb))
   }
+
 }
+
 
