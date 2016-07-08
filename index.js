65726f29b36b797cd8deb1175d2c02e309dfb1be
Dominic Tarr <dominic.tarr@gmail.com>
2016-07-08 22:06:27 +1200

    pass either a keypair or a seed

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 var handshake = require('./handshake')
 var secure = require('./secure')
+var cl = require('chloride')
+
+function isBuffer(buf, len) {
+  return Buffer.isBuffer(buf) && buf.length === len
+}
+
+function toKeys(seed) {
+  if(isBuffer(seed.publicKey, 32) && isBuffer(seed.secretKey, 64)) return seed
+  if(isBuffer(seed, 32))
+    return cl.crypto_sign_seed_keypair(seed)
+  throw new Error('keypair or seed must be provided')
+}
 
 exports.client =
 exports.createClient = function (alice, app_key, timeout) {
-  var create = handshake.client(alice, app_key, timeout)
+  var create = handshake.client(toKeys(alice), app_key, timeout)
 
   return function (bob, cb) {
-    if(!Buffer.isBuffer(bob) || bob.length != 32)
+    if(!isBuffer(bob, 32))
       throw new Error('createClient *must* be passed a public key')
     return create(bob, secure(cb))
   }
@@ -14,12 +26,15 @@ exports.createClient = function (alice, app_key, timeout) {
 }
 exports.server =
 exports.createServer = function (bob, authorize, app_key, timeout) {
-  var create = handshake.server(bob, authorize, app_key, timeout)
+  var create = handshake.server(toKeys(bob), authorize, app_key, timeout)
 
   return function (cb) {
     return create(secure(cb))
   }
 
 }
+
+
+
 
 
