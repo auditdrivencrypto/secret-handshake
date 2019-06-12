71f7b81aafb1bc4a7cf165a344f1847b6e73dff0
mixmix <whimful@gmail.com>
2019-06-12 22:13:04 +1200

    add debugging to testsuite server test

--- test/shs1-test/server.js
+++ test/shs1-test/server.js
@@ -5,63 +5,31 @@ const toPull = require('stream-to-pull-stream')
 const { createServerStream } = require('../..')
 
 const appKey = Buffer.from(process.argv[2], 'hex')
-const authorize = (pubKey, cb) => cb(null, true) // all clients are allowed to connect
 const bob = { // the keypair of the server
-  publicKey: Buffer.from(process.argv[4], 'hex'),
-  secretKey: Buffer.from(process.argv[3], 'hex')
+  secretKey: Buffer.from(process.argv[3], 'hex'),
+  publicKey: Buffer.from(process.argv[4], 'hex')
 }
+const authorize = (pubKey, cb) => cb(null, true) // all clients are allowed to connect
 const timeout = 30 // I hope this is milliseconds?
 
 const ServerStream = createServerStream(bob, authorize, appKey, timeout) // duplex handshake stream
+var d = Date.now()
 const stream = ServerStream((err, plainStream) => {
-  console.log(err, plainStream)
+  if (err) process.stderr.write(`X ${d} ERROR ${err}\n`)
+  else process.stderr.write(`0 ${d} DONE\n`)
+
+  process.kill(process.pid)
+  // is this sufficient to kill this process? are the streams closed?
+})
+
+process.on('SIGTERM', () => {
+  process.stderr.write(`X ${d} received SIGTERM\n`) // triggered by process.kill above
 })
 
 pull(
   toPull.source(process.stdin),
+  pull.through(data => process.stderr.write(`> ${d}\n`)),
   stream,
+  pull.through(data => process.stderr.write(`< ${d}\n`)),
   toPull.sink(process.stdout)
 )
-
-//
-// const { verifyMsg1, createMsg2, verifyMsg3, createMsg4, serverOutcome } = require('../index.js')
-// const serverState = {
-//   shsStep: 1, // which message are you receiving next
-//   network_identifier: Buffer.from(process.argv[2], 'hex'),
-//   server_longterm_sk: Buffer.from(process.argv[3], 'hex'),
-//   server_longterm_pk: Buffer.from(process.argv[4], 'hex'),
-//   server_ephemeral_sk: Buffer.from([176, 248, 210, 185, 226, 76, 162, 153, 239, 144, 57, 206, 218, 97, 2, 215, 155, 5, 223, 189, 22, 28, 137, 85, 228, 233, 93, 79, 217, 203, 63, 125]),
-//   server_ephemeral_pk: Buffer.from([166, 12, 63, 218, 235, 136, 61, 99, 232, 142, 165, 147, 88, 93, 79, 177, 23, 148, 129, 57, 179, 24, 192, 174, 90, 62, 40, 83, 51, 9, 97, 82])
-// }
-
-// process.stdin.on('readable', () => {
-//   switch (serverState.shsStep) {
-//     case 1: {
-//       const msg1 = process.stdin.read()
-
-//       if (!verifyMsg1(serverState, msg1)) process.exit(1)
-
-//       process.stdout.write(createMsg2(serverState))
-//       serverState.shsStep = 3
-//       break
-//     }
-//     case 3: {
-//       const msg3 = process.stdin.read()
-
-//       if (!verifyMsg3(serverState, msg3)) {
-//         process.exit(3)
-//       }
-
-//       const outcome = serverOutcome(serverState)
-//       process.stdout.write(Buffer.concat([
-//         createMsg4(serverState),
-//         outcome.encryption_key,
-//         outcome.encryption_nonce,
-//         outcome.decryption_key,
-//         outcome.decryption_nonce
-//       ]))
-//       break
-//     }
-//     default:
-//   }
-// })
