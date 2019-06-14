c2e1ffcb31c44db0603c75840692cc1d0e1331ea
mixmix <whimful@gmail.com>
2019-06-14 12:54:46 +1200

    shs1-test server tests working !

--- test/shs1-test/server.js
+++ test/shs1-test/server.js
@@ -10,21 +10,21 @@ const bob = { // the keypair of the server
   publicKey: Buffer.from(process.argv[4], 'hex')
 }
 const authorize = (pubKey, cb) => cb(null, true) // all clients are allowed to connect
-const timeout = 30 // I hope this is milliseconds?
+const timeout = 500 // I hope this is milliseconds!
 
 const shake = createServer(bob, authorize, appKey, timeout)((err, stream) => {
   if (err) {
     log(`! ${err}`)
-    // shs1-test : If the server detects that the client is not well-behaved, it must immediately exit with a non-zero exit code, without writing any further data to stdout.
     process.exit(1)
-  } else {
-    const { encryptKey, decryptKey, encryptNonce, decryptNonce } = stream.crypto
-    const result = Buffer.concat([ encryptKey, encryptNonce, decryptKey, decryptNonce ])
-    log(`< writing ${result.length} bytes (final)`)
-    process.stdout.write(result)
-
-    // process.kill(process.pid)
+    // shs1-test : If the server detects that the client is not well-behaved,
+    // it must immediately exit with a non-zero exit code, without writing any further data to stdout.
   }
+
+  const { encryptKey, decryptKey, encryptNonce, decryptNonce } = stream.crypto
+  const result = Buffer.concat([ encryptKey, encryptNonce, decryptKey, decryptNonce ])
+
+  log(`< writing ${result.length} bytes (final)`)
+  process.stdout.write(result)
 })
 
 process.on('SIGTERM', () => {
@@ -41,5 +41,5 @@ pull(
 
 function log (string) {
   // uncomment this to get some debugging data
-  process.stderr.write(`PID ${process.pid}: ${string}\n`)
+  // process.stderr.write(`PID ${process.pid}: ${string}\n`)
 }
